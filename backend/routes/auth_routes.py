ee9bf8b771de669ce125686ca479264c9192c470
proparieur <proparieur88@gmail.com>
2025-06-02 15:51:18 +0000

    Update auth_routes.py

--- backend/routes/auth_routes.py
+++ backend/routes/auth_routes.py
@@ -1,1 +1,28 @@
+from flask import Blueprint, render_template, redirect, url_for, flash, request
+from flask_login import login_user, logout_user, login_required
+from models import User
+from werkzeug.security import check_password_hash
+from app import db
 
+auth_bp = Blueprint('auth', __name__)
+
+@auth_bp.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+        user = User.query.filter_by(email=email).first()
+        if user and check_password_hash(user.password, password):
+            login_user(user)
+            flash('Connexion réussie', 'success')
+            return redirect(url_for('user.dashboard'))
+        else:
+            flash('Identifiants incorrects', 'danger')
+    return render_template('login.html')
+
+@auth_bp.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    flash("Déconnexion réussie", "success")
+    return redirect(url_for('auth.login'))
