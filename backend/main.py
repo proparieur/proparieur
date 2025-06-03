a8db073812288e3167c0735f158ddb76a6d6ac42
proparieur <proparieur88@gmail.com>
2025-06-03 19:55:50 +0000

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -18,7 +18,7 @@ login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = 'login'
 
-# MODELES
+# MODELES (exemples simples, à adapter)
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(150), unique=True, nullable=False)
